14ee2842c400fb24a051a0409ed4c985c6a750e9
Eduard <eduard.torres.romero@me.com>
2024-10-30 18:12:18 +0100

    spinner v5

--- web/static/assets/js/main.js
+++ web/static/assets/js/main.js
@@ -1,6 +1,7 @@
 <script>
 
     if (document.readyState == "complete") {
+        console.log("Page loaded!")
         document.getElementById("loading-spinner").style.display = "none";
     }
     
