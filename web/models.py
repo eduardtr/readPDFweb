dba265465d922ae8341d48a1e6decf456a0ca031
Eduard <eduard.torres.romero@me.com>
2024-10-30 21:21:25 +0100

    fileextensionlimitator pdf

--- web/models.py
+++ web/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class Question(models.Model):
     question = models.CharField(max_length=5000)
-    file = models.FileField(upload_to='documents/', blank=True)
+    file = models.FileField(upload_to='documents/', blank=True, validators=[FileExtensionValidator(['pdf'])])
     answer = models.CharField(max_length=5000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
